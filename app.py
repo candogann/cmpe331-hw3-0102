780e3815ebaceecef243b9ae6bd34bf988ea97c6
candogann <101297290+candogann@users.noreply.github.com>
2022-12-02 11:55:09 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -6,10 +6,17 @@ Stage : Development -01
 """
 
 from bs4 import BeautifulSoup
-import json
+import requests
 
-with open('./ssd.html', encoding="utf-8") as f:
-    soup = BeautifulSoup(f,'html.parser')
+
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import requests
+
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
+url='https://www.nytimes.com/section/science'
+response=requests.get(url,headers=headers)
+soup = BeautifulSoup(response.text,'html.parser')
 
 tags = soup.find_all('h2')
 for i in tags:
